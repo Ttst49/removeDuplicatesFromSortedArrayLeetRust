3403a48fb99d7b904172558b14d57586d99c6794
Thibaut <thibautstachnick@gmail.com>
2024-06-01 12:09:28 +0200

    solve the problem

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+pub fn remove_duplicates(nums: &mut Vec<i32>) -> i32 {
+    nums.dedup();
+    nums.len() as i32
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut nums = vec![1,1,2];
+    remove_duplicates(&mut nums);
 }
